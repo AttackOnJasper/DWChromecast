9cc49ccbbff8ac5c936076ac797b8e461953d943
AttackOnJasper <wangdc2010@gmail.com>
2015-10-01 19:10:06 -0400

    Set up

---
+++ Pod/Classes/ChromecastManager.h
@@ -0,0 +1,13 @@
+//
+//  ChromecastManager.h
+//  Pods
+//
+//  Created by Jasper Wang on 15/10/1.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ChromecastManager : NSObject
+
+@end
---
+++ Pod/Classes/ChromecastManager.m
@@ -0,0 +1,13 @@
+//
+//  ChromecastManager.m
+//  Pods
+//
+//  Created by Jasper Wang on 15/10/1.
+//
+//
+
+#import "ChromecastManager.h"
+
+@implementation ChromecastManager
+
+@end
