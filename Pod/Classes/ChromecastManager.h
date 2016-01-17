0e877cfd46e0cdae30d1e1df4a25f65f07c3752c
AttackOnJasper <wangdc2010@gmail.com>
2016-01-17 02:53:02 -0500

    update library

--- Pod/Classes/ChromecastManager.h
+++ Pod/Classes/ChromecastManager.h
@@ -7,10 +7,110 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <GoogleCast/GCKCastChannel.h>
+#import <GoogleCast/GCKDeviceScanner.h>
+#import <GoogleCast/GCKMediaStatus.h>
 
-@interface ChromecastManager : NSObject
+@class GCKDevice;
+@class GCKDeviceManager;
+@class GCKMediaControlChannel;
+@class GCKMediaInformation;
 
-@property (strong, nonatomic) GCKCastChannel *castChannel;
+#pragma mark - CastDeviceControllerDelegate
+
+@protocol CastDeviceControllerDelegate <NSObject>
+
+@optional
+
+/**
+ * Called when connection to the device was established.
+ *
+ * @param device The device to which the connection was established.
+ */
+- (void)didConnectToDevice:(GCKDevice *)device;
+
+/**
+ *  Called when the device disconnects.
+ */
+- (void)didDisconnect;
+
+/**
+ *  Initialize device scan
+ */
+
+- (void)initScan:(NSString *)applicationID;
+
+/**
+ * Called when Cast devices are discoverd on the network.
+ */
+- (void)didDiscoverDeviceOnNetwork;
+
+/**
+ * Called when the connected device's queue updates.
+ */
+- (void)didUpdateQueueForDevice:(GCKDevice *)device;
+
+/**
+ *  Called when the next item in a queue starts preloading.
+ *
+ *  @param item GCKMediaQueueItem
+ */
+- (void)didUpdatePreloadStatusForItem:(GCKMediaQueueItem *)item;
+
+/**
+ *  Whether or not the device controller should be displayed.
+ *
+ *  @return YES to display, NO to prevent.
+ */
+- (BOOL)shouldDisplayModalDeviceController;
 
 @end
+
+#pragma mark - Interface
+
+@interface ChromecastManager : NSObject <GCKDeviceScannerListener>
+
++ (instancetype)sharedInstance;
+
+@property(nonatomic, weak) id<CastDeviceControllerDelegate> delegate;
+
+/**
+ *  The device manager used to manage a connection to a Cast device.
+ */
+@property(nonatomic, strong) GCKDeviceManager *deviceManager;
+
+/**
+ *  The device scanner used to detect devices on the network.
+ */
+@property(nonatomic, strong) GCKDeviceScanner *deviceScanner;
+
+/**
+ *  The media control channel for the playing media.
+ */
+@property(nonatomic, strong) GCKMediaControlChannel *mediaControlChannel;
+
+/**
+ *  Display the media currently being cast.
+ */
+- (void)displayCurrentlyPlayingMedia;
+
+/**
+ *  Sets the position of the playback on the Cast device.
+ *
+ *  @param newPercent 0.0-1.0
+ */
+- (void)setPlaybackPercent:(float)newPercent;
+
+/**
+ *  Connect to the given Cast device.
+ *
+ *  @param device A GCKDevice from the deviceScanner list.
+ */
+- (void)connectToDevice:(GCKDevice *)device;
+
+/**
+ *  Enable basic logging of all GCKLogger messages to the console.
+ */
+- (void)enableLogging;
+
+@end
+
--- Pod/Classes/ChromecastManager.m
+++ Pod/Classes/ChromecastManager.m
@@ -7,7 +7,62 @@
 //
 
 #import "ChromecastManager.h"
+#import <GoogleCast/GoogleCast.h>
+
+@interface ChromecastManager() <GCKDeviceManagerDelegate,
+GCKLoggerDelegate, GCKMediaControlChannelDelegate>
+
+@property (strong, nonatomic) NSString *applicationID;
+
+@end
 
 @implementation ChromecastManager
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+- (void)dealloc {
+
+}
+
+#pragma mark - Public methods
+
++ (instancetype)sharedInstance {
+    static dispatch_once_t p = 0;
+    __strong static id _sharedChromecastManager = nil;
+    
+    dispatch_once(&p, ^{
+        _sharedChromecastManager = [[self alloc] init];
+    });
+    
+    return _sharedChromecastManager;
+}
+
+- (void)initScan:(NSString *)applicationID {
+    self.applicationID = applicationID;
+    
+//    GCKFilterCriteria *filterCriteria = [GCKFilterCriteria criteriaForAvailableApplicationWithID: applicationID];
+//    
+//    self.deviceScanner = [[GCKDeviceScanner alloc] initWithFilterCriteria:filterCriteria];
+//    
+//    [self.deviceScanner addListener:self];
+//    [self.deviceScanner startScan];
+    
+}
+
+#pragma mark - Private helpers
+
+
+#pragma mark - GCKDeviceManagerDelegate
+
+#pragma mark - GCKLoggerDelegate
+
+#pragma mark - GCKMediaControlChannelDelegate
+
 
 @end
